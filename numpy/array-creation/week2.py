fda3370227fc36a3b0aeda76db34f45d3b02b5d2
Pedrono <annaig.pedrono@imft.fr>
2020-04-28 17:40:56 +0200

    exo

---
+++ numpy/array-creation/week2.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+list1=(1, 2., 5, 6, 6.3)
+array1=np.array(list1)
+print(array1)
+
+# 1D numpy
+array2 = np.arange(-2.0,2.2,0.2)
+print(array2)
+
+# linspace
+array3 = np.linspace(0.5,1.5,11)
+print(array3)
+
+# string
+string = 'mP3R56AD'
+array_str = np.array(string,'c')
+print(array_str)
+
+
+
